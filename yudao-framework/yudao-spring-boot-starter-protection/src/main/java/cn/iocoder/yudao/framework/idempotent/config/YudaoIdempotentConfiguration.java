e2325f9934df5f51ae42ed97ffc184763fe0f164
igloo_marc <jianming.zeng@dopchina.com>
2023-01-25 20:29:40 +0800

    feat: first init

---
+++ yudao-framework/yudao-spring-boot-starter-protection/src/main/java/cn/iocoder/yudao/framework/idempotent/config/YudaoIdempotentConfiguration.java
@@ -0,0 +1,42 @@
+package cn.iocoder.yudao.framework.idempotent.config;
+
+import cn.iocoder.yudao.framework.idempotent.core.aop.IdempotentAspect;
+import cn.iocoder.yudao.framework.idempotent.core.keyresolver.IdempotentKeyResolver;
+import cn.iocoder.yudao.framework.idempotent.core.keyresolver.impl.DefaultIdempotentKeyResolver;
+import cn.iocoder.yudao.framework.idempotent.core.keyresolver.impl.ExpressionIdempotentKeyResolver;
+import cn.iocoder.yudao.framework.idempotent.core.redis.IdempotentRedisDAO;
+import cn.iocoder.yudao.framework.redis.config.YudaoRedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.List;
+
+@AutoConfiguration
+@AutoConfigureAfter(YudaoRedisAutoConfiguration.class)
+public class YudaoIdempotentConfiguration {
+
+    @Bean
+    public IdempotentAspect idempotentAspect(List<IdempotentKeyResolver> keyResolvers, IdempotentRedisDAO idempotentRedisDAO) {
+        return new IdempotentAspect(keyResolvers, idempotentRedisDAO);
+    }
+
+    @Bean
+    public IdempotentRedisDAO idempotentRedisDAO(StringRedisTemplate stringRedisTemplate) {
+        return new IdempotentRedisDAO(stringRedisTemplate);
+    }
+
+    // ========== 各种 IdempotentKeyResolver Bean ==========
+
+    @Bean
+    public DefaultIdempotentKeyResolver defaultIdempotentKeyResolver() {
+        return new DefaultIdempotentKeyResolver();
+    }
+
+    @Bean
+    public ExpressionIdempotentKeyResolver expressionIdempotentKeyResolver() {
+        return new ExpressionIdempotentKeyResolver();
+    }
+
+}
